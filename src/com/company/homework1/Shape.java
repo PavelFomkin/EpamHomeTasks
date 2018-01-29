cb3fea1db8ed02a89a89fa441c36ef592dc600aa
Pavel Fomkin <pavel_foma@mail.ru>
2018-01-29 13:17:00 +0300

    attempt1 IDEA

--- src/com/company/homework1/Shape.java
+++ src/com/company/homework1/Shape.java
@@ -6,5 +6,7 @@ public class Shape {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
         System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
     }
 }
